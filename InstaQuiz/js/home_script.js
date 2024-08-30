6c345aa9c066613828ef115045483431975f28fb
Secrettttttt <francisco.paulo.pf@gmail.com>
2024-08-30 21:30:30 +0800

    Updated home_script.js to fix signup button redirection issue

--- InstaQuiz/js/home_script.js
+++ InstaQuiz/js/home_script.js
@@ -3,3 +3,12 @@ document.getElementById("get-started").addEventListener("click", function(event)
     window.location.href = "../html/signin.html";
 
 });
+
+ 
+document.getElementById("signup").addEventListener("click", function(event) {
+    event.preventDefault();  
+    window.location.href = "../html/signup.html";
+});
+
+
+
