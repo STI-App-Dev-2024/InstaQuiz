19ec5abaafa98e841888939aeefdeb3489f0af86
Secrettttttt <francisco.paulo.pf@gmail.com>
2024-09-02 12:52:00 +0800

    minor changes

--- InstaQuiz/js/nav_script.js
+++ InstaQuiz/js/nav_script.js
@@ -1,29 +1,31 @@
-document.getElementById("logo").addEventListener("click", function (event) {
-    event.preventDefault();
-    window.location.href = "../html/home.html";
-});
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("logo").addEventListener("click", function (event) {
+        event.preventDefault();
+        window.location.href = "../html/home.html";
+    });
 
-document.getElementById("nav_home").addEventListener("click", function (event) {
-    event.preventDefault();
-    window.location.href = "../html/home.html";
-});
+    document.getElementById("nav_home").addEventListener("click", function (event) {
+        event.preventDefault();
+        window.location.href = "../html/home.html";
+    });
 
-document.getElementById("nav_contact").addEventListener("click", function (event) {
-    event.preventDefault();
-    window.location.href = "../html/contact.html";
-});
+    document.getElementById("nav_contact").addEventListener("click", function (event) {
+        event.preventDefault();
+        window.location.href = "../html/contact.html";
+    });
 
-document.getElementById("nav_faq").addEventListener("click", function (event) {
-    event.preventDefault();
-    window.location.href = "../html/faq.html";
-});
+    document.getElementById("nav_faq").addEventListener("click", function (event) {
+        event.preventDefault();
+        window.location.href = "../html/faq.html";
+    });
 
-document.getElementById("login").addEventListener("click", function (event) {
-    event.preventDefault();
-    window.location.href = "../html/signin.html";
-});
+    document.querySelector(".login").addEventListener("click", function (event) {
+        event.preventDefault();
+        window.location.href = "../html/signin.html";
+    });
 
-document.getElementById("signup").addEventListener("click", function (event) {
-    event.preventDefault();
-    window.location.href = "../html/signup.html";
+    document.querySelector(".signup").addEventListener("click", function (event) {
+        event.preventDefault();
+        window.location.href = "../html/signup.html";
+    });
 });
