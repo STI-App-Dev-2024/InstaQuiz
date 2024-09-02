26e748ba71663e62387c07758cca8fffe150339e
Secrettttttt <francisco.paulo.pf@gmail.com>
2024-09-02 12:52:59 +0800

    separate signin script

---
+++ InstaQuiz/js/signin_script.js
@@ -0,0 +1,56 @@
+
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
+import { getAuth, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
+
+
+// Import and configure Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDoLbJdlLUsfMp09PwA_31TYcetC3JxIWs",
+    authDomain: "instaquiz-37134.firebaseapp.com",
+    projectId: "instaquiz-37134",
+    storageBucket: "instaquiz-37134.appspot.com",
+    messagingSenderId: "78078580689",
+    appId: "1:78078580689:web:0184ebf9d3896a3b20b2f0"
+};
+
+ 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+function showMessage(message, divId) {
+  const messageDiv = document.getElementById(divId);
+  messageDiv.style.display = "block";
+  messageDiv.innerHTML = message;
+  messageDiv.style.opacity = 1;
+  setTimeout(() => {
+    messageDiv.style.opacity = 0;
+  }, 5000);
+}
+
+document.getElementById('signin').addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        showMessage('Sign in successful', 'signInMessage');
+        const user = userCredential.user;
+        localStorage.setItem('loggedInUserId', user.uid);
+        window.location.href = 'homepage.html'; // Redirect to your home page or dashboard
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        if (errorCode === 'auth/wrong-password') {
+          showMessage('Wrong password', 'signInMessage');
+        } else if (errorCode === 'auth/user-not-found') {
+          showMessage('No user found with this email', 'signInMessage');
+        } else {
+          showMessage('Error during sign-in: ' + error.message, 'signInMessage');
+        }
+      });
+});
+
+  
